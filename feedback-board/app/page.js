c7a816627c98e50739ed5ab26efcf123fbaeac19
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2023-12-05 09:34:23 +0000

    modals and popup
    built the modals for adding a feedback and conditioned printing feedbacks.

--- feedback-board/app/page.js
+++ feedback-board/app/page.js
@@ -1,6 +1,31 @@
+'use client';
+import { useState } from "react";
+import FeedbackFormPopup from "./components/FeedbackFormPopup";
 import FeedbackItem from "./components/FeedbackItem";
+import Button from "./components/Button";
 
 export default function Home() {
+  const [showFeebackPopupForm, setShowFeedbackPopupForm] = useState(false)
+  const [showFeebackPopupItem, setShowFeedbackPopupItem] = useState(null)
+  function openFeedbackPopUpForm(){
+    setShowFeedbackPopupForm(true)
+  }
+
+  function openFeedbackPopupItem(feedback){
+    setShowFeedbackPopupItem(feedback)
+  }
+
+  const feedbacks = [
+    { title:'Please post more videos', 
+      description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod impedit sequi officiis consequuntur repudiandae facilis recusandae architecto voluptates laborum explicabo?', 
+      votesCount: 80
+    },
+    { title:'Please post more stories', 
+      description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod impedit sequi officiis consequuntur repudiandae facilis recusandae architecto voluptates laborum explicabo?', 
+      votesCount: 40
+    }
+  ]
+
   return (
     <main className="bg-white max-w-2xl md:mx-auto 
         md:shadow-lg md:rounded-lg md:mt-8 overflow-hidden">
@@ -15,19 +40,30 @@ export default function Home() {
       <div className="bg-gray-100 px-8 py-4 flex border-b">
         <div className="grow"></div>
         <div>
-          <button className="bg-blue-500 py-1 px-4 rounded-md 
-            text-white text-opacity-90">
+          <Button
+            primary
+            onClick={openFeedbackPopUpForm} >
             Make a suggestion
-          </button>
+          </Button>
         </div>
       </div>
       <div className="px-8 ">
-        <FeedbackItem />  
-        <FeedbackItem />  
-        <FeedbackItem />  
-        <FeedbackItem />  
-        <FeedbackItem />  
+        {feedbacks.map(feedback => (
+          <FeedbackItem {...feedback} 
+                        onOpen={()=> openFeedbackPopupItem(feedback)}/> 
+        ))}
+         
+         
       </div>
+      {showFeebackPopupForm && (
+        <FeedbackFormPopup setShow={setShowFeedbackPopupForm}/>
+      )}
+      {showFeebackPopupItem && (
+        <div>
+          opened feedback: {showFeebackPopupItem.title}
+        </div>
+      )}
+
     </main>
   )
 }
