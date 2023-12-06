83223908ac4828756eb5144b9a76a629c83a1873
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2023-12-06 18:43:45 +0000

    comment popup build

--- feedback-board/app/components/FeedbackItemPopup.js
+++ feedback-board/app/components/FeedbackItemPopup.js
@@ -1,23 +1,23 @@
 import Button from "./Button";
 import Popup from "./Popup";
-
+import FeedbackItemPopupComments from "./FeedbackItemPopupComments";
 export default function FeedbackItemPopup({title,description, setShow, children, votesCount }){
   return(
     <Popup title={''} setShow={setShow}> 
       
-      <div className="p-8">
+      <div className="p-8 pb-2">
         <h2 className="text-lg font-bold mb-2"> {title} </h2>
-        <p>{description}</p>
+        <p className="text-gray-600">{description}</p>
         
       </div>
-      <div className="flex justify-end bg-gray-200 px-8 py-2">
+      <div className="flex justify-end  px-8 py-2 border-b">
         <Button primary>
           <span className="triangle-vote-up"></span>
           Upvote {votesCount}
         </Button>
       </div>
       <div>
-        comments
+        <FeedbackItemPopupComments />
       </div>
     </Popup>
   )
---
+++ feedback-board/app/components/FeedbackItemPopupComments.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Button from "./Button";
+import Avatar from "./Avatar";
+
+export default function FeedbackItemPopupComments(){
+  const [commentText, setCommentText]= useState('');
+
+
+  return(
+    <div className="p-8">
+    <div className="flex gap-4 mb-8">
+      <Avatar />
+      <div>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+         Perferendis eligendi reiciendis, impedit velit facere omnis!
+         Quasi possimus reprehenderit neque, sint obcaecati ipsum magni
+         modi impedit quae odio corrupti accusantium veniam? 
+      </p>
+      <div className="text-gray-400 mt-2 text-sm">
+        Anonymous &middot; a few seconds ago
+      </div>
+      </div>
+    </div>
+    <form action="">
+      <textarea
+      onChange={(ev)=> setCommentText(ev.target.value)} 
+      value={commentText}
+      className="border rounded-md w-full p-2"
+      placeholder="let us know what you think" />
+    
+      <div className="flex justify-end gap-2 mt-2">
+        <Button>Attach Filed</Button>
+        <Button primary disabled={commentText===''}>Comment</Button>
+      </div>
+    </form>
+    </div>
+  )
+}
